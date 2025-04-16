558e4daadc82930702f8f5058054d93d61613d4e
John Smith <johnsmith0x3f@gmail.com>
2025-04-16 08:03:12 +0000

    feat: init kernel as a process

--- main/pkg/kernel/src/proc/mod.rs
+++ main/pkg/kernel/src/proc/mod.rs
@@ -35,7 +35,7 @@ pub fn init() {
     trace!("Init kernel vm: {:#?}", proc_vm);
 
     // kernel process
-    let kproc = { /* FIXME: create kernel process */ };
+    let kproc = Process::new(String::from("kernel"), None, Some(proc_vm), Some(ProcessData::new()));
     manager::init(kproc);
 
     info!("Process Manager Initialized.");
