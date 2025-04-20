b3b3fb960ea8fd7f959cae942db8e12ba10d8da2
John Smith <johnsmith0x3f@gmail.com>
2025-04-20 11:25:08 +0000

    feat: update log format

--- main/pkg/kernel/src/utils/logger.rs
+++ main/pkg/kernel/src/utils/logger.rs
@@ -1,4 +1,6 @@
-use log::{Metadata, Record};
+use log::{Level, Metadata, Record};
+
+use owo_colors::OwoColorize;
 
 pub fn init() {
     static LOGGER: Logger = Logger;
@@ -19,14 +21,32 @@ impl log::Log for Logger {
 
     fn log(&self, record: &Record) {
         // DONE: Implement the logger with serial output
-        let prefix = match record.level() {
-            log::Level::Error => "\x1B[1;31mERROR:",
-            log::Level::Warn => "\x1B[1;33mWARNING:",
-            log::Level::Info => "\x1B[0;32mINFO:",
-            log::Level::Debug => "\x1B[0;37mDEBUG:",
-            log::Level::Trace => "\x1B[0;30mTRACE:",
-        };
-        println!("{} {} \x1B[0;39m", prefix, record.args());
+        //let prefix = match record.level() {
+        //    log::Level::Error => "\x1B[1;31mERROR:",
+        //    log::Level::Warn => "\x1B[1;33mWARNING:",
+        //    log::Level::Info => "\x1B[0;32mINFO:",
+        //    log::Level::Debug => "\x1B[0;37mDEBUG:",
+        //    log::Level::Trace => "\x1B[0;30mTRACE:",
+        //};
+        //println!("{} {} \x1B[0;39m", prefix, record.args());
+
+        match record.level() {
+            Level::Error => {
+                println!("{} {}", "[x]".bold().red(), record.args().red());
+            }
+            Level::Warn => {
+                println!("{} {}", "[!]".bold().yellow(), record.args().yellow());
+            }
+            Level::Info => {
+                println!("{} {}", "[+]".bold().green(), record.args().green());
+            }
+            Level::Debug => {
+                println!("{} {}", "[D]".bold().blue(), record.args().blue());
+            }
+            Level::Trace => {
+                println!("{} {}", "[_]".bold().dimmed(), record.args().dimmed());
+            }
+        }
     }
 
     fn flush(&self) {}
