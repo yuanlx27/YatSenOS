2ad91c4d1f364f6a045da9e15992ad307c1a0929
John Smith <johnsmith0x3f@gmail.com>
2025-05-19 14:47:24 +0000

    feat: implement counter with both semaphore and spinlock

--- main/pkg/app/counter/src/main.rs
+++ main/pkg/app/counter/src/main.rs
@@ -4,13 +4,29 @@
 use lib::*;
 extern crate lib;
 
+use spin::*;
+
 const THREAD_COUNT: usize = 8;
 static mut COUNTER: isize = 0;
 
-static MUTEX: Semaphore = Semaphore::new(0xDEADBEEF);
+static SEMPH: Semaphore = Semaphore::new(0xDEADBEEF);
+static SPLOK: Mutex<SpinLock> = Mutex::new(SpinLock::new());
 
 fn main() -> isize {
-    MUTEX.init(1);
+    let pid = sys_fork();
+
+    if pid == 0 {
+        test_semaphore();
+    } else {
+        sys_wait_pid(pid);
+        test_spinlock();
+    }
+
+    0
+}
+
+fn test_semaphore() {
+    SEMPH.init(1);
     let mut pids = [0u16; THREAD_COUNT];
 
     for i in 0..THREAD_COUNT {
@@ -33,16 +49,44 @@ fn main() -> isize {
     }
 
     println!("COUNTER result: {}", unsafe { COUNTER });
+}
 
-    0
+fn test_spinlock() {
+    let mut splok = SPLOK.lock();
+    let mut pids = [0u16; THREAD_COUNT];
+
+    for i in 0..THREAD_COUNT {
+        let pid = sys_fork();
+        if pid == 0 {
+            for _ in 0..100 {
+                splok.acquire();
+                inc_counter();
+                splok.release();
+            }
+            sys_exit(0);
+        } else {
+            pids[i] = pid; // only parent knows child's pid
+        }
+    }
+
+    let cpid = sys_get_pid();
+    println!("process #{} holds threads: {:?}", cpid, &pids);
+    sys_stat();
+
+    for i in 0..THREAD_COUNT {
+        println!("#{} waiting for #{}...", cpid, pids[i]);
+        sys_wait_pid(pids[i]);
+    }
+
+    println!("COUNTER result: {}", unsafe { COUNTER });
 }
 
 fn do_counter_inc() {
     for _ in 0..100 {
         // DONE: protect the critical section
-        MUTEX.wait();
+        SEMPH.wait();
         inc_counter();
-        MUTEX.signal();
+        SEMPH.signal();
     }
 }
 
