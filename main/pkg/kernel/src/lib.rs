6f2faf83e4e387a6f86ae49efdcc1a05db0bc9de
John Smith <johnsmith0x3f@gmail.com>
2025-06-17 02:28:11 +0000

    feat: kernel stack auto grow

--- main/pkg/kernel/src/lib.rs
+++ main/pkg/kernel/src/lib.rs
@@ -54,6 +54,30 @@ pub fn init(boot_info: &'static BootInfo) {
     info!("Interrupts Enabled.");
 
     info!("YatSenOS initialized.");
+
+    info!("Test stack grow.");
+    grow_stack();
+    info!("Stack grow test done.");
+}
+
+#[inline(never)]
+#[unsafe(no_mangle)]
+pub fn grow_stack() {
+    const STACK_SIZE: usize = 1024 * 4;
+    const STEP: usize = 64;
+
+    let mut array = [0u64; STACK_SIZE];
+    info!("Stack: {:?}", array.as_ptr());
+
+    // test write
+    for i in (0..STACK_SIZE).step_by(STEP) {
+        array[i] = i as u64;
+    }
+
+    // test read
+    for i in (0..STACK_SIZE).step_by(STEP) {
+        assert_eq!(array[i], i as u64);
+    }
 }
 
 pub fn wait(pid: proc::ProcessId) {
